a1fd3a9576e99010bd6ee07bd61b0061be742897
HianeCoelho <137707058+HianeCoelho@users.noreply.github.com>
2024-10-05 22:14:03 -0300

    temporizador tartaruga

--- Inimigos.h
+++ Inimigos.h
@@ -16,8 +16,8 @@ public:
 	bool vivo;
 
 	Inimigos() :
-			deslocamento(0.0f, 0.0f), posicaoInimigos(0.0f, 0.0f), onGround(false), vivo(
-					true) {
+			deslocamento(0.0f, 0.0f), posicaoInimigos(0.0f, 0.0f), onGround(
+					false), vivo(true) {
 		gravity = 0.5f;
 		speed = 50.0f;
 	}
@@ -71,6 +71,16 @@ public:
 		}
 	}
 
+	void morrer() {
+		speed = 0.0f;
+		vivo = false;
+	}
+
+	void renascer() {
+			speed = 60.0f;
+			vivo = false;
+		}
+
 	void morrerDefinitivamente() {
 		imagemInimigos.setPosition(0.0f, 0.0f);
 
@@ -81,6 +91,9 @@ public:
 };
 
 class Tartaruga: public Inimigos {
+private:
+	sf::Clock clock;
+	sf::Time time;
 public:
 	Tartaruga() {
 		Inimigos();
@@ -95,138 +108,154 @@ public:
 	}
 
 	void morrer() {
-		speed = 0.0f;
-		vivo = false;
+		Inimigos::morrer();
+		clock.restart();
 		imagemInimigos.setTextureRect(sf::IntRect(270, 336, 25, 15));
 	}
 	void renascer() {
-		speed = 60.0f;
-		vivo = true;
+		Inimigos::renascer();
 		imagemInimigos.setTextureRect(sf::IntRect(7, 331, 15, 15));
-	}
-
-};
-
-class Caranguejo: public Inimigos {
-public:
-	Caranguejo() {
-		Inimigos();
-		imagemInimigos.setTextureRect(sf::IntRect(6, 422, 16, 13));
-		imagemInimigos.scale(2.5, 2.5);
-	}
-	void desenharCaranguejo(sf::RenderWindow &window) {
-		carregarTexture();
-		window.draw(imagemInimigos);
-	}
-	void morrer() {
-		speed = 0.0f;
-		vivo = false;
-		imagemInimigos.setTextureRect(sf::IntRect(132, 418, 16, 16));
-	}
-	void renascer() {
-		speed = 60.0f;
-		vivo = true;
-		imagemInimigos.setTextureRect(sf::IntRect(6, 422, 16, 13));
-	}
-
-};
-
-class Vagalume: public Inimigos {
-public:
-	Vagalume() {
-		Inimigos();
-		imagemInimigos.setTextureRect(sf::IntRect(6, 509, 15, 12));
-		imagemInimigos.scale(2, 2);
-	}
-	void desenharVagalume(sf::RenderWindow &window) {
-		carregarTexture();
-		window.draw(imagemInimigos);
-	}
-	void pular() {
-		if (onGround == true and posicao.x < 720) {
-			deslocamento.y = -3.0f;
-			onGround = false;
-		}
-	}
-	void morrer() {
-		speed = 0.0f;
-		vivo = false;
-		imagemInimigos.setTextureRect(sf::IntRect(154, 507, 18, 16));
-	}
-	void renascer() {
-		speed = 60.0f;
-		vivo = true;
-		imagemInimigos.setTextureRect(sf::IntRect(6, 509, 15, 12));
-	}
-
-};
-
-class Fogo: GameObject {
-protected:
-	sf::Texture textureInimigos;
-	sf::Sprite imagemInimigos;
-	sf::Vector2f posicaoFogo;
-	sf::Vector2f velocity;
-	float gravity;
-	float speed;
-public:
-	int vx, vy;
-	Fogo() :
-			posicaoFogo(0.0f, 0.0f), velocity(0.0f, 0.0f) {
-		textureInimigos.loadFromFile("sprites.png");
-		imagemInimigos.setTexture(textureInimigos);
-		imagemInimigos.setTextureRect(sf::IntRect(10, 216, 16, 16));
-		imagemInimigos.scale(3, 3);
-		imagemInimigos.setPosition(posicaoFogo);
-		gravity = 0.5f;
-		speed = 150.0f;
-		vx = vy = 0;
-	}
-	void desenharFogo(sf::RenderWindow &window) {
-		window.draw(imagemInimigos);
 	}
 
 	void update(float deltaTime,
 			const std::vector<std::vector<int>> &collisionMap, float cellWidth,
 			float cellHeight) override {
-		velocity.x = 0.0f; // Inicializa a velocidade horizontal
-		// Atualiza a velocidade horizontal com base no tempo
-		velocity.x = deltaTime * speed;
-		// Aplica a gravidade
-		velocity.y += 0;
-		// Move a forma do inimigo com base na velocidade
-		posicao = imagemInimigos.getPosition(); // Obtém a posição atual do jogador
-		globalBounds = imagemInimigos.getGlobalBounds();
-		width = globalBounds.width;        // Obtém a largura
-		height = globalBounds.height;        // Obtém a altura
-//Coordena a movimentação na horizontal
-		if (posicao.x + width < 0) {
-			velocity.x = 0;
-			speed = 150.0f;
-			velocity.x = speed * deltaTime;
-		} else if (posicao.x > 800 - width) {
-			velocity.x = 0;
-			speed = -150.0f;
-			velocity.x = speed * deltaTime;
-		}
-//Coordena a movimentação na vertical
-		if (posicao.x <= 1 and speed == -150.0f) {
-			if (posicao.y == 0) {
-				imagemInimigos.setPosition(0, 170);
+		if (vivo == false) {
+			if (clock.getElapsedTime() > sf::seconds(5.0f)) {
+				std::cout << "TEmpo" << std::endl;
+				renascer();
+				clock.restart();
 			}
-
-		} else if (posicao.x <= 5 and speed == -150.0f and posicao.y == 170) {
-			imagemInimigos.setPosition(0, 275);
-		} else if (posicao.x <= 5 and speed == -150.0f and posicao.y == 275) {
-			imagemInimigos.setPosition(0, 430);
-		} else if (posicao.x <= 5 and speed == -150.0f and posicao.y == 430) {
-			imagemInimigos.setPosition(0, 0);
+		} else {
+			Inimigos::update(deltaTime, collisionMap, cellWidth, cellHeight);
 		}
-
-		imagemInimigos.move(velocity);
 
 	}
-
 };
+
+	class Caranguejo: public Inimigos {
+	public:
+		Caranguejo() {
+			Inimigos();
+			imagemInimigos.setTextureRect(sf::IntRect(6, 422, 16, 13));
+			imagemInimigos.scale(2.5, 2.5);
+		}
+		void desenharCaranguejo(sf::RenderWindow &window) {
+			carregarTexture();
+			window.draw(imagemInimigos);
+		}
+		void morrer() {
+			speed = 0.0f;
+			vivo = false;
+			imagemInimigos.setTextureRect(sf::IntRect(132, 418, 16, 16));
+		}
+		void renascer() {
+			speed = 60.0f;
+			vivo = true;
+			imagemInimigos.setTextureRect(sf::IntRect(6, 422, 16, 13));
+		}
+
+	};
+
+	class Vagalume: public Inimigos {
+	public:
+		Vagalume() {
+			Inimigos();
+			imagemInimigos.setTextureRect(sf::IntRect(6, 509, 15, 12));
+			imagemInimigos.scale(2, 2);
+		}
+		void desenharVagalume(sf::RenderWindow &window) {
+			carregarTexture();
+			window.draw(imagemInimigos);
+		}
+		void pular() {
+			if (onGround == true and posicao.x < 720) {
+				deslocamento.y = -3.0f;
+				onGround = false;
+			}
+		}
+		void morrer() {
+			speed = 0.0f;
+			vivo = false;
+			imagemInimigos.setTextureRect(sf::IntRect(154, 507, 18, 16));
+		}
+		void renascer() {
+			speed = 60.0f;
+			vivo = true;
+			imagemInimigos.setTextureRect(sf::IntRect(6, 509, 15, 12));
+		}
+
+	};
+
+	class Fogo: GameObject {
+	protected:
+		sf::Texture textureInimigos;
+		sf::Sprite imagemInimigos;
+		sf::Vector2f posicaoFogo;
+		sf::Vector2f velocity;
+		float gravity;
+		float speed;
+	public:
+		int vx, vy;
+		Fogo() :
+				posicaoFogo(0.0f, 0.0f), velocity(0.0f, 0.0f) {
+			textureInimigos.loadFromFile("sprites.png");
+			imagemInimigos.setTexture(textureInimigos);
+			imagemInimigos.setTextureRect(sf::IntRect(10, 216, 16, 16));
+			imagemInimigos.scale(3, 3);
+			imagemInimigos.setPosition(posicaoFogo);
+			gravity = 0.5f;
+			speed = 150.0f;
+			vx = vy = 0;
+		}
+		void desenharFogo(sf::RenderWindow &window) {
+			window.draw(imagemInimigos);
+		}
+
+		void update(float deltaTime,
+				const std::vector<std::vector<int>> &collisionMap,
+				float cellWidth, float cellHeight) override {
+			velocity.x = 0.0f; // Inicializa a velocidade horizontal
+			// Atualiza a velocidade horizontal com base no tempo
+			velocity.x = deltaTime * speed;
+			// Aplica a gravidade
+			velocity.y += 0;
+			// Move a forma do inimigo com base na velocidade
+			posicao = imagemInimigos.getPosition(); // Obtém a posição atual do jogador
+			globalBounds = imagemInimigos.getGlobalBounds();
+			width = globalBounds.width;        // Obtém a largura
+			height = globalBounds.height;        // Obtém a altura
+//Coordena a movimentação na horizontal
+			if (posicao.x + width < 0) {
+				velocity.x = 0;
+				speed = 150.0f;
+				velocity.x = speed * deltaTime;
+			} else if (posicao.x > 800 - width) {
+				velocity.x = 0;
+				speed = -150.0f;
+				velocity.x = speed * deltaTime;
+			}
+//Coordena a movimentação na vertical
+			if (posicao.x <= 1 and speed == -150.0f) {
+				if (posicao.y == 0) {
+					imagemInimigos.setPosition(0, 170);
+				}
+
+			} else if (posicao.x <= 5 and speed == -150.0f
+					and posicao.y == 170) {
+				imagemInimigos.setPosition(0, 275);
+			} else if (posicao.x <= 5 and speed == -150.0f
+					and posicao.y == 275) {
+				imagemInimigos.setPosition(0, 430);
+			} else if (posicao.x <= 5 and speed == -150.0f
+					and posicao.y == 430) {
+				imagemInimigos.setPosition(0, 0);
+			}
+
+			imagemInimigos.move(velocity);
+
+		}
+
+	};
 
 #endif /* INIMIGOS_H_ */
--- main.cpp
+++ main.cpp
@@ -16,3 +16,4 @@ int main() {
 
 	return EXIT_SUCCESS; // Retorna sucesso na execução
 }
+
